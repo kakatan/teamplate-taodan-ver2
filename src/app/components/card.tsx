8f101576deceef68f9e7ec09b3f3a37892a584c8
LongNKT <congnc@tuonglaigroup.vn>
2023-06-02 16:05:29 +0700

    commit lần 2

---
+++ src/app/components/card.tsx
@@ -0,0 +1,19 @@
+"use client";
+import styles from '../styles/card.module.scss';
+const Card = () => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.content}>
+        <p className={styles.heading}>Card Hover Effect</p>
+        <p className={styles.para}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
+          laboriosam at voluptas minus culpa deserunt delectus sapiente
+          inventore pariatur
+        </p>
+        <button className={styles.btn}>Read more</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,78 +3,33 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Card from "./components/card";
+
 import React, { useEffect } from "react";
 
 export default function Home() {
   return (
     <>
-     <Header />
-     
-        <div className={styles.main}>
-          
+     <Header />   
+       
+         <div className={styles.main}>          
             <Image
               className={styles.logo}
               src="/logotaodan.png"
               alt="Next.js Logo"
-              width={400}
-              height={100}
+              width={300}
+              height={80}
               priority
-            />
+            />         
+          <section className={styles.grid}>        
+             <Card/>
+             <Card/>
+             <Card/>
+             <Card/>
+          
          
-          <section className={styles.grid}>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>Dịch vụ</h2>
-              <li>Dịch vụ khám chữa bệnh</li>             
-              <li>Dịch vụ xét nghiệm, chuẩn đoán hình ảnh</li>             
-              <li>Dịch vụ chăm sóc điều trị</li>             
-              <li>Dịch vụ tại nhà</li>
-            </a>
-
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>Bác sĩ</h2>              
-              <li>Dịch vụ khám chữa bệnh</li>             
-              <li>Dịch vụ xét nghiệm, chuẩn đoán hình ảnh</li>             
-              <li>Dịch vụ chăm sóc điều trị</li>             
-              <li>Dịch vụ tại nhà</li>
-            </a>
-
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>Hoạt động - sự kiện</h2>             
-              <li>Dịch vụ khám chữa bệnh</li>             
-              <li>Dịch vụ xét nghiệm, chuẩn đoán hình ảnh</li>             
-              <li>Dịch vụ chăm sóc điều trị</li>             
-              <li>Dịch vụ tại nhà</li>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>Hỗ trợ</h2>              
-              <li>Dịch vụ khám chữa bệnh</li>             
-              <li>Dịch vụ xét nghiệm, chuẩn đoán hình ảnh</li>             
-              <li>Dịch vụ chăm sóc điều trị</li>             
-              <li>Dịch vụ tại nhà</li>
-            </a>
           </section>
-        </div>
+        </div> 
      
     
       <Footer />
